4e65f3436cb5c3081708f9e47fe8a07510a85f0f
juanroj <juanger143@yahoo.com>
2023-04-26 15:29:08 -0400

    ejemplos de Math

---
+++ math.js
@@ -0,0 +1,11 @@
+Math.pow(3,3); /*Este método nos permite potenciar un número.
+el primero es la base y el segundo será la potencia.*/
+Math.round(0.49);//redondeará el número decimal al entero más próximo (0)
+Math.floor(5.93);// redondeará el número decimal al entero de menor valor (5)
+Math.ceil(3.27);/*este método, al contrario que el anterior, 
+redondeará un número al siguiente entero (4)*/
+Math.max(1,2,3,4,5);//Este método nos permitirá conocer el valor máximo (5)
+Math.min(1,2,3,4,5);//Este método nos permitirá conocer el valor minimo (1)
+Math.random(); /*Este último método nos permitirá generar un número aleatorio. 
+//Es importante tener en cuenta que el número que se generará es decimal
+puede ser cualquiera entre el 0 y el 1*/
