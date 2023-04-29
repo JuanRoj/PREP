588e6423cfb9f95c2246214d4bb01de16b8cdb90
juanroj <juanger143@yahoo.com>
2023-04-28 21:06:50 -0400

    Bucles

---
+++ bucles.js
@@ -0,0 +1,31 @@
+var suma = 0;
+suma = suma + 1;
+suma = suma + 2;
+suma = suma + 3;
+suma = suma + 4;
+console.log(suma)
+
+var suma =0;
+for (var i=0 ; i<10; i++){
+    suma =suma+1;
+    console.log(suma);
+}
+var suma =0;
+for (var i=0 ; i<10; i++){
+    suma =suma+i;
+    console.log("Variable de Iteracción" , i);
+}
+console.log("Variable suma: ", suma)
+
+var suma=0
+for (let i = 0; i < 5; i++) {
+    suma = suma + i;
+    console.log('Variable de iteración: ', i);
+ }
+ console.log('Variable suma: ', suma);
+
+ var suma=0
+ while(suma < 8) {
+    suma = suma + 1;
+        console.log(suma);
+    };
